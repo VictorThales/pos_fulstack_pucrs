f7140e4f0478aa1f51824666856e7a291cc17e78
Victor Thales <42248030+VictorThales@users.noreply.github.com>
2023-06-19 21:50:58 -0300

    Update poo.js

--- poo.js
+++ poo.js
@@ -21,3 +21,26 @@ function createAndroid (type, velocity, cpu, state) {
 };
 
 const android1 = createAndroid('original',150,2.5,true);
+
+//constructor
+
+function Android() {
+    this.type = "Original";
+    this.velocity = 100;
+    this.state = true;
+    this.cpu = "2.7";
+    this.stateOn = function () {
+        this.state = true;
+    };
+    this.stateOff = function () {
+        this.state = false;
+    };
+     this.speedUp = function () {
+        this.velocity = this.velocity + 50;
+    };
+    this.speedDown = function () {
+        this.velocity = this.velocity - 50;
+    };
+}
+
+const android2 = new Android;
